2587ed3711ca318191ee41621127c5684fbbd6f7
Jerome <jeromelesne@gmail.com>
2024-02-15 16:09:03 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,163 @@
+const trainerContainer = document.querySelector("#trainerContainer");
+const editTrainerWin = document.querySelector("#editTrainer");
+const addTrainerWin = document.querySelector("#addTrainer");
+const editSend = document.querySelector("#editSend");
+const formEdit = document.querySelector("#formEdit");
+const addSend = document.querySelector("#addSend");
+const formAdd = document.querySelector("#formAdd");
+const addTrainerBtn = document.querySelector("#addTrainerBtn");
+
+addTrainerBtn.addEventListener("click", () => {
+    addTrainerWin.classList.remove("hidden");
+});
+
+addSend.addEventListener("click", () => {
+    addTrainer();
+    addTrainerWin.classList.add("hidden");
+});
+
+editSend.addEventListener("click", () => {
+    editTrainer(formEdit.getAttribute("data-trainerId"));
+    editTrainerWin.classList.add("hidden");
+});
+
+async function getTrainer() {
+    const response = await fetch("http://127.0.0.1:3000/trainer", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    const data = await response.json();
+    return data;
+}
+
+async function addTrainer() {
+    const response = await fetch("http://127.0.0.1:3000/trainer", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: document.querySelector("#nameAdd").value,
+            age: document.querySelector("#ageAdd").value,
+        }),
+    });
+    console.log(await response.json());
+    displayTrainers();
+}
+
+async function deleteTrainer(id) {
+    const response = await fetch(`http://127.0.0.1:3000/trainer/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    console.log(await response.json());
+    displayTrainers();
+}
+
+async function editTrainer(id) {
+    const response = await fetch(`http://127.0.0.1:3000/trainer/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: document.querySelector("#nameEdit").value,
+            age: document.querySelector("#ageEdit").value,
+        }),
+    });
+    console.log(await response.json());
+    displayTrainers();
+}
+
+async function detailTrainer(id) {
+    const response = await fetch(`http://127.0.0.1:3000/trainer-poke/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    const data = await response.json();
+    return data.pokemon;
+}
+
+async function displayTrainers() {
+    trainerContainer.innerHTML = "";
+    const trainer = await getTrainer();
+    trainer.forEach(async (e) => {
+        const article = document.createElement("article");
+        const title = document.createElement("h3");
+        const age = document.createElement("p");
+        const modifyBtn = document.createElement("button");
+        const deleteBtn = document.createElement("button");
+        const detailBtn = document.createElement("button");
+        const btnContainer = document.createElement("div");
+        const pokeContainer = document.createElement("div");
+        pokeContainer.classList.add("pokeContainer");
+        modifyBtn.innerHTML = "Modify";
+        modifyBtn.addEventListener("click", () => {
+            editTrainerWin.classList.remove("hidden");
+            formEdit.setAttribute("data-trainerId", e._id);
+        });
+        deleteBtn.innerHTML = "Delete";
+        deleteBtn.addEventListener("click", () => {
+            deleteTrainer(e._id);
+        });
+        detailBtn.innerHTML = "Detail";
+        detailBtn.addEventListener(
+            "click",
+            async () => {
+                const poke = await detailTrainer(e._id);
+                console.log(poke);
+                poke.map((el) => {
+                    const pokemon = document.createElement("div");
+                    pokemon.classList.add("pokemon");
+
+                    const pokeName = document.createElement("p");
+                    pokeName.classList.add("pokeName");
+                    pokeName.innerHTML = el.name;
+
+                    const pokeType = document.createElement("p");
+                    pokeType.innerHTML = el.type;
+
+                    const pokeLvl = document.createElement("p");
+                    pokeLvl.innerHTML = el.level;
+
+                    const pokeAtk = document.createElement("div");
+                    const pokeAtkTitle = document.createElement("h3");
+                    pokeAtkTitle.innerHTML = "Attacks :";
+                    pokeAtk.appendChild(pokeAtkTitle);
+                    el.attack.map((atk) => {
+                        const attack = document.createElement("p");
+                        attack.innerHTML = atk;
+                        pokeAtk.appendChild(attack);
+                    });
+
+                    pokemon.appendChild(pokeName);
+                    pokemon.appendChild(pokeType);
+                    pokemon.appendChild(pokeLvl);
+                    pokemon.appendChild(pokeAtk);
+
+                    pokeContainer.appendChild(pokemon);
+                });
+            },
+            { once: true },
+        );
+        btnContainer.classList.add("btnContainer");
+        btnContainer.appendChild(modifyBtn);
+        btnContainer.appendChild(deleteBtn);
+        btnContainer.appendChild(detailBtn);
+        title.textContent = e.name;
+        age.textContent = e.age;
+        article.appendChild(title);
+        article.appendChild(age);
+        article.appendChild(btnContainer);
+        article.appendChild(pokeContainer);
+        trainerContainer.appendChild(article);
+    });
+}
+
+displayTrainers();
